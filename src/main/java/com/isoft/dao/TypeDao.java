8b257208164ae9e79590f962d708480ed37cfa3a
morning <977898691@qq.com>
2019-06-30 16:51:01 +0800

    morningstorm:TypeDao的selectAll和IncreaseVariety方法

---
+++ src/main/java/com/isoft/dao/TypeDao.java
@@ -0,0 +1,16 @@
+package com.isoft.dao;
+
+import com.isoft.entity.TbType;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+public interface TypeDao {
+//    添加商品类别
+    @Insert("insert into tb_type values(null,#{lbname})")
+    int IncreaseVariety(String lbname);
+//    查询所有商品类别
+    @Select("select * from tb_type")
+    List<TbType> selectAll();
+}
