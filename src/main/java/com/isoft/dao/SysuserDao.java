1e46e526cad44f96317e76a5b90b90732fe703c6
LiBai <1183157230@qq.com>
2019-07-03 10:10:45 +0800

    Sysuser

--- src/main/java/com/isoft/dao/SysuserDao.java
+++ src/main/java/com/isoft/dao/SysuserDao.java
@@ -5,6 +5,7 @@ import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
@@ -17,20 +18,30 @@ import java.util.List;
  * 修改密码
  * 查看所有管理员
  */
+@Repository
 public interface SysuserDao {
     /**
      * 添加管理员
      */
     @Insert("insert into tb_sysuser " +
-            "values(null , #{sname} , #{spass} , #{smobile} , 1 , #{Srole})")
+            "values(null ,#{sid}, #{sname} , #{spass} , #{smobile} , 1 , #{srole});")
     int add(TbSysuser user) ;
+
+    /**
+     * 查看所有管理员
+     */
+    @Select("select * from tb_sysuser")
+    List<TbSysuser> getAll();
 
     /**
      * 逻辑删除-离职
      * @Param state:0--离职，1--在职
      */
-    @Update("update tb_sysuser set status=#{status} where id = #{id}")
-    int updateState(@Param("id") int id , @Param("status") int status);
+    @Update("update tb_sysuser set status=1 where id = #{id}")
+    int addState(@Param("id") int id);
+
+    @Update("update tb_sysuser set status=0 where id = #{id}")
+    int delState(@Param("id") int id);
 
     /**
      * 根据id将多个人的状态设置为离职
@@ -45,8 +56,8 @@ public interface SysuserDao {
     /**
      * 登录校验
      */
-    @Select("select * from tb_sysuser where sname=#{sname} and spass=#{spass} and state=1")
-    TbSysuser login(@Param("sname") String sname, @Param("spass") String spass) ;
+    @Select("select * from tb_sysuser where sid=#{sid} and spass=#{spass} and status=1")
+    TbSysuser login(@Param("sid") String sid, @Param("spass") String spass) ;
 
     /**
      * 修改密码
@@ -54,9 +65,4 @@ public interface SysuserDao {
     @Update("update tb_sysuser set spass=#{newPass} where id=#{id}")
     int repass(@Param("id") Integer id, @Param("newPass") String newPass) ;
 
-    /**
-     * 查看所有管理员
-     */
-    @Select("select * from tb_sysuser")
-    List<TbSysuser> getAll();
 }
